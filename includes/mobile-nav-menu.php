9613ccd7de2d85387ac9b6924edd84a57af27fb5
Shasheesha Janith Dissanayake <85898263+shasheesha@users.noreply.github.com>
2023-12-28 20:32:32 +0530

    fixing

--- includes/mobile-nav-menu.php
+++ includes/mobile-nav-menu.php
@@ -1,43 +1,23 @@
 <?php
 
-session_start();
 
-$isShopper = false;
-$addCart = "";
-$isUserLogged = false;
-$loggedUser = null;
 
-if(isset($_SESSION["user"])){
-    $loggedUser = $_SESSION["user"];
-    $isUserLogged = true;
-    if($loggedUser["acc_type"] === ACCOUNT_TYPE_SHOPPER){
-        $isShopper = true;
-        $addCart = "disabled";
-    }
-}
+
 
 ?>
-
-<nav id="nav">
-        <div class="container">
-            <a href="index.php">
-            <img src="./assets/Mr.PC.png" id="nav-logo" alt="Mr.PC" srcset="">
-            </a>
-
-            <button class="btn btn-primary ham-button" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobile-menu" aria-controls="mobile-menu">
-            <svg fill="none" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m19 11h-18c-.265216 0-.51957-.1054-.707107-.2929-.187536-.1875-.292893-.4419-.292893-.7071 0-.26522.105357-.51957.292893-.70711.187537-.18753.441891-.29289.707107-.29289h18c.2652 0 .5196.10536.7071.29289.1875.18754.2929.44189.2929.70711 0 .2652-.1054.5196-.2929.7071s-.4419.2929-.7071.2929zm0-7h-18c-.265216 0-.51957-.10536-.707107-.29289-.187536-.18754-.292893-.44189-.292893-.70711s.105357-.51957.292893-.70711c.187537-.18753.441891-.29289.707107-.29289h18c.2652 0 .5196.10536.7071.29289.1875.18754.2929.44189.2929.70711s-.1054.51957-.2929.70711c-.1875.18753-.4419.29289-.7071.29289zm0 14h-18c-.265216 0-.51957-.1054-.707107-.2929-.187536-.1875-.292893-.4419-.292893-.7071s.105357-.5196.292893-.7071c.187537-.1875.441891-.2929.707107-.2929h18c.2652 0 .5196.1054.7071.2929s.2929.4419.2929.7071-.1054.5196-.2929.7071-.4419.2929-.7071.2929z" fill="rgb(0,0,0)"/></svg>
-            </button>
-
-            <ul class="nav-items">
-                <li id="home-page"><a href="./index.php">Home</a></li>
-                <li id="shop-online-page"><a href="./online-shop.php">Products</a></li>
-                <li id="contact-page"><a href="./contact-us.php">Contact</a></li>
-                <li id="about-page"><a href="./about-us.php">About Us</a></li>
-            </ul>
-            <?php
+<div class="offcanvas offcanvas-top" id="mobile-menu" tabindex="-1" id="mobile-menu" aria-labelledby="mobile-menu">
+    <div class="offcanvas-body">
+        <ul class="mobile-nav-items">
+            <li id="home-page"><a href="./index.php">Home</a></li>
+            <li id="shop-online-page"><a href="./online-shop.php">Products</a></li>
+            <li id="contact-page"><a href="./contact-us.php">Contact</a></li>
+            <li id="about-page"><a href="./about-us.php">About Us</a></li>
+        </ul>
+        
+        <?php
                 if(!isset($_SESSION["user"])){
             ?>
-            <div id="nav-sign">
+            <div id="mobile-nav-sign">
                 <!-- <input type="button" class="cus-btn primary" id="sign-up" value="Sign up" data-bs-toggle="offcanvas" data-bs-target="#sign-up-offcanvasRight" aria-controls="sign-up-offcanvasRight"> -->
                 <button class="cus-btn secondary" id="cart-btn" value="cart" data-bs-toggle="offcanvas" data-bs-target="#s-cart-offcanvasRight" aria-controls="s-cart-offcanvasRight">
                     <img src="./assets/icons/shopping-cart2.svg" alt="" width="20" height="20">
@@ -61,7 +41,7 @@ if(isset($_SESSION["user"])){
             <?php }elseif(isset($_SESSION["user"]) && $_SESSION["user"]['acc_type']== ACCOUNT_TYPE_SHOPPER) {
 
                     ?>
-                    <div id="nav-sign" style="cursor: pointer" onclick="location.href='./shopper/admin-shopper.php'">
+                    <div id="mobile-nav-sign" style="cursor: pointer" onclick="location.href='./shopper/admin-shopper.php'">
                         <img style="width: 40px; height: 40px; border-radius: 100%; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
                         <h6 style="margin: 0px;">Welcome, <?php echo $_SESSION['user']['first_name'] ?></h6>
                     </div>
@@ -70,7 +50,7 @@ if(isset($_SESSION["user"])){
                 }elseif(isset($_SESSION["user"]) && $_SESSION["user"]['acc_type']== ACCOUNT_TYPE_CUSTOMER) {
 
                     ?>
-                    <div id="nav-sign" style="cursor: pointer" onclick="location.href='./customer/customer-orders.php'">
+                    <div id="mobile-nav-sign" style="cursor: pointer" onclick="location.href='./customer/customer-orders.php'">
                         <img style="width: 40px; height: 40px; border-radius: 100%; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
                         <h6 style="margin: 0px;">Welcome, <?php echo $_SESSION['user']['first_name'] ?></h6>
                     </div>
@@ -79,7 +59,7 @@ if(isset($_SESSION["user"])){
                 }elseif(isset($_SESSION["user"]) && $_SESSION["user"]['acc_type']== ACCOUNT_TYPE_ADMIN) {
 
                     ?>
-                    <div id="nav-sign" style="cursor: pointer; display: flex; flex-direction: row; gap:10px;" onclick="location.href='./admin/admin-dashboard.php'">
+                    <div id="mobile-nav-sign" style="cursor: pointer; display: flex; flex-direction: row; gap:10px;" onclick="location.href='./admin/admin-dashboard.php'">
                         <img style="width: 40px; height: 40px; border-radius: 100%; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
                         <h6 style="margin: 0px;">Welcome, <?php echo $_SESSION['user']['first_name'] ?></h6>
                     </div>
@@ -87,9 +67,5 @@ if(isset($_SESSION["user"])){
                     <?php
                 }
             ?>
-        </div>
-        
-</nav>
-<?php include"./includes/mobile-nav-menu.php" ?>
-
-<script src="./js/bootstrap.bundle.js"></script>
+    </div>
+</div>

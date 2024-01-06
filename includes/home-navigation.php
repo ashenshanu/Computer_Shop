9c2ee1c318cb66a1a9da160d99a89cefd0f27f30
Shasheesha Janith Dissanayake <85898263+shasheesha@users.noreply.github.com>
2024-01-06 14:24:46 +0530

    Update home-navigation.php

--- includes/home-navigation.php
+++ includes/home-navigation.php
@@ -61,9 +61,23 @@ if(isset($_SESSION["user"])){
             <?php }elseif(isset($_SESSION["user"]) && $_SESSION["user"]['acc_type']== ACCOUNT_TYPE_SHOPPER) {
 
                     ?>
-                    <div id="nav-sign" style="cursor: pointer" onclick="location.href='./shopper/admin-shopper.php'">
-                        <img style="width: 40px; height: 40px; border-radius: 100%; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
-                        <h6 style="margin: 0px;">Welcome, <?php echo $_SESSION['user']['first_name'] ?></h6>
+                    <div id="nav-sign">
+                        
+                        <div class="dropdown search-feild">
+                            <button class="cus-btn secondary dropdown-toggle" id="search-feild-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="./assets/icons/search.svg" alt="" width="20" height="20">
+                            </button>
+                            <ul class="dropdown-menu">
+                                <div class="search-bar">
+                                    <input type="search" class="search" name="Search" placeholder="Search Here Shop or Product" id="search_head" onkeyup="handleSearchFiled(event)">
+                                    <input type="button" id="search-btn" class="search-btn btn primary" value="Search" onclick="handleSearchButton(document.getElementById('search_head').value)">
+                                </div>
+                            </ul>
+                        </div>
+                        <div id="nav-sign" style="cursor: pointer" onclick="location.href='./shopper/admin-shopper.php'">
+                            <img style="width: 40px; height: 40px; border-radius: 100%; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
+                            <h6 style="margin: 0px;">Welcome, <?php echo $_SESSION['user']['first_name'] ?></h6>
+                        </div>
                     </div>
 
                     <?php
@@ -71,25 +85,25 @@ if(isset($_SESSION["user"])){
 
                     ?>
                     <div id="nav-sign">
-                <!-- <input type="button" class="cus-btn primary" id="sign-up" value="Sign up" data-bs-toggle="offcanvas" data-bs-target="#sign-up-offcanvasRight" aria-controls="sign-up-offcanvasRight"> -->
-                <button class="cus-btn secondary" id="cart-btn" value="cart" data-bs-toggle="offcanvas" data-bs-target="#s-cart-offcanvasRight" aria-controls="s-cart-offcanvasRight">
-                    <img src="./assets/icons/shopping-cart2.svg" alt="" width="20" height="20">
-                </button>
-                <div class="dropdown search-feild">
-                    <button class="cus-btn secondary dropdown-toggle" id="search-feild-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="./assets/icons/search.svg" alt="" width="20" height="20">
-                    </button>
-                    <ul class="dropdown-menu">
-                        <div class="search-bar">
-                            <input type="search" class="search" name="Search" placeholder="Search Here Shop or Product" id="search_head" onkeyup="handleSearchFiled(event)">
-                            <input type="button" id="search-btn" class="search-btn btn primary" value="Search" onclick="handleSearchButton(document.getElementById('search_head').value)">
+                        <!-- <input type="button" class="cus-btn primary" id="sign-up" value="Sign up" data-bs-toggle="offcanvas" data-bs-target="#sign-up-offcanvasRight" aria-controls="sign-up-offcanvasRight"> -->
+                        <button class="cus-btn secondary" id="cart-btn" value="cart" data-bs-toggle="offcanvas" data-bs-target="#s-cart-offcanvasRight" aria-controls="s-cart-offcanvasRight">
+                            <img src="./assets/icons/shopping-cart2.svg" alt="" width="20" height="20">
+                        </button>
+                        <div class="dropdown search-feild">
+                            <button class="cus-btn secondary dropdown-toggle" id="search-feild-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="./assets/icons/search.svg" alt="" width="20" height="20">
+                            </button>
+                            <ul class="dropdown-menu">
+                                <div class="search-bar">
+                                    <input type="search" class="search" name="Search" placeholder="Search Here Shop or Product" id="search_head" onkeyup="handleSearchFiled(event)">
+                                    <input type="button" id="search-btn" class="search-btn btn primary" value="Search" onclick="handleSearchButton(document.getElementById('search_head').value)">
+                                </div>
+                            </ul>
                         </div>
-                    </ul>
-                </div>
-                <button class="cus-btn secondary" id="nav-sign" style="cursor: pointer" onclick="location.href='./customer/customer-orders.php'">
-                    <img style="width: 40px; height: 40px; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
-                </button>
-            </div>
+                        <button class="cus-btn secondary" id="nav-sign" style="cursor: pointer" onclick="location.href='./customer/customer-orders.php'">
+                            <img style="width: 40px; height: 40px; object-fit: cover;" onerror="this.src='./assets/product.jpg'" src='./uploads/user_images/<?php echo $_SESSION['user']['dp_img']?>' alt='Card image cap'>
+                        </button>
+                    </div>
                     <?php
                 }elseif(isset($_SESSION["user"]) && $_SESSION["user"]['acc_type']== ACCOUNT_TYPE_ADMIN) {
 
